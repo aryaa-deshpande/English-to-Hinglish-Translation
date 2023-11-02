c32078adc103db4fe20538c95c7c26fbe33e29fc
aryaa-deshpande <140228656+aryaa-deshpande@users.noreply.github.com>
2023-11-02 21:51:10 +0530

    Add files via upload

---
+++ words.py
@@ -0,0 +1,23 @@
+
+word =  [
+    "the", "be", "to", "of", "and", "a", "in", "that", "have", "I",
+    "it", "for", "not", "on", "with", "he", "as", "you", "do", "at",
+    "this", "but", "his", "by", "from", "wait","they", "we", "say", "her", "she",
+    "or", "an", "will", "my", "one", "all", "would", "there", "their", "what",
+    "so", "up", "out", "if", "about", "who", "get", "which", "go", "me",
+    "when", "make", "can", "like", "time", "no", "just", "him", "know", "take",
+    "people", "into", "year", "your", "good", "some", "could", "them", "see", "other",
+    "than", "then", "now", "look", "only", "come", "its", "over", "think", "also",
+    "back", "after", "use", "two", "how", "our", "work", "first", "well", "way",
+    "even", "new", "want", "because", "any", "these", "give", "day", "most", "us",
+    "is", "are", "was", "were", "it's", "that's", "you're", "I'm", "doesn't", "won't",
+    "can't", "didn't", "he's", "she's", "let's", "there's", "we're", "you've", "isn't", "aren't",
+    "they're", "haven't", "we'll", "I'll", "we've", "you'd", "I'd", "I've", "hasn't", "hadn't",
+    "could've", "should've", "would've", "it'll", "shouldn't", "wouldn't", "they've", "could've", "you'll",
+    "who's", "what's", "where's", "when's", "why's", "how's", "will", "can", "would", "shall", "should",
+    "could", "might", "may", "must", "ought", "need", "will not", "can't", "won't", "I will", "I am",
+    "share", "products", "productions", "waiting", "bag", "big", "video", "supers",
+    "car", "house", "tree", "cat", "dog", "bird", "food", "water", "computer", "phone",
+    "book", "pen", "desk", "chair", "shoes", "hat", "shirt", "pants", "bicycle", "music",
+    "movie", "guitar", "beach", "ocean", "mountain", "city", "park", "child", "friend", "love"
+]
